ca9d10f518e30dfa78edb4e2bd5b4cf7e57a442d
I <henrikl@tuta.io>
2022-06-05 08:59:30 +0200

    adds mm

--- units.js
+++ units.js
@@ -1,8 +1,9 @@
 import { displayFunctionsThatUsesDimenUnits } from "./displayData.js"
 
 export class DimenUnitHandler{
-    static avalibleUnits = ["px", "rem"]
+    static avalibleUnits = ["px", "rem", "mm"]
     static currentUnit = "px"
+    static #mmMeasurementDiv = document.getElementById("mmMeasurement")
 
     static changeUnit(newUnit){
         DimenUnitHandler.currentUnit = newUnit
@@ -24,8 +25,15 @@ export class DimenUnitHandler{
 			return px/getFontSize()
 		} else if(DimenUnitHandler.currentUnit == "px"){
 			return px
-		}
+		} else if (DimenUnitHandler.currentUnit == "mm"){
+            return DimenUnitHandler.pxToMm(px)
+        }
 		throw new Error("Invalid Unit")
+    }
+    static pxToMm(px){
+        const heightStr = document.defaultView.getComputedStyle(DimenUnitHandler.#mmMeasurementDiv).height
+        const heightInPx = heightStr.substring(0, heightStr.length-2)
+        return Math.floor(px/(heightInPx/100));
     }
 }
 
